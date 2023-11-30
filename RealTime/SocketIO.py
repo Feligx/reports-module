d9de6f2736b619061d94924504d9bca6f585d3ab
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2023-11-30 18:50:23 -0500

    socketio

--- RealTime/SocketIO.py
+++ RealTime/SocketIO.py
@@ -1,19 +1,65 @@
+import json
+import time
+import eventlet
 from SocketService import SocketService
+import logging
+import socketio
+
+logging.basicConfig(level=logging.INFO)
 
 
 class SocketIO(SocketService):
     def __init__(self):
         super().__init__()
+        self.app = None
+        self.sio = socketio.Server()
         self.payload = None
+        self.connections = {}
 
-    def on_connect(self):
-        pass
+    def find_connection_by_user_id(self, user_id, data):
+        logging.info("SocketIO: Updating payload")
+        user_sid = self.connections[user_id]
+        self.notify(self.payload, user_sid)
 
-    def on_disconnect(self):
-        pass
+    def notify(self, sid, user_sid):
+        logging.info("SocketIO: Notifying clients")
+        self.sio.emit('notify', json.dumps(self.payload), room=user_sid)
 
-    def update(self):
-        pass
+    def run(self, host='localhost', port=5000):
+        @self.sio.event
+        def receive_report(sid, data):
+            self.payload = json.dumps(data)
+            self.find_connection_by_user_id(sid, data)
 
-    def notify(self):
+        @self.sio.event
+        def user_connected(sid, data):
+            self.connections[data["user_id"]] = sid
+
+        @self.sio.event
+        def connect(sid, environ):
+            print(f"Client {sid} connected to /notifications")
+
+        @self.sio.event
+        def disconnect(sid):
+            print(f"Client {sid} disconnected from /notifications")
+
+        # Use eventlet to run the Socket.IO server
+        app = socketio.WSGIApp(self.sio)
+        eventlet.wsgi.server(eventlet.listen((host, port)), app)
+
+        # Event handler for "/receive_report"
+
+
+if __name__ == "__main__":
+    socket_server = SocketIO()
+    socket_server.run()
+
+    try:
+        while True:
+            # Simulate some ongoing process
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # Gracefully handle KeyboardInterrupt to disconnect clients
         pass
+    finally:
+        socket_server.sio.disconnect()
--- RealTime/WebSocket.py
+++ RealTime/WebSocket.py
@@ -1,19 +1,35 @@
+import json
+
 from SocketService import SocketService
+import logging
+# use websockets instead of socketio
+from websockets import serve
+import asyncio
+
+logging.basicConfig(level=logging.INFO)
 
 
 class WebSocket(SocketService):
-    def __init__(self):
+    def __init__(self, host, port):
         super().__init__()
+        self.host = host
+        self.port = port
         self.payload = None
 
-    def on_connect(self):
-        pass
+    async def notify(self, websocket):
+        async for message in websocket:
+            logging.info("WebSocket: Updating payload")
+            self.payload = json.loads(message)
+            await websocket.send(json.dumps(self.payload))
 
-    def on_disconnect(self):
-        pass
+    async def start_server(self):
+        logging.info("WebSocket: Starting server")
+        async with serve(self.notify, self.host, self.port):
+            await asyncio.Future()
 
-    def update(self):
-        pass
 
-    def notify(self):
-        pass
+if __name__ == '__main__':
+    ws_server = WebSocket("localhost", 8765)
+
+    # Start the WebSocket server
+    asyncio.run(ws_server.start_server())
